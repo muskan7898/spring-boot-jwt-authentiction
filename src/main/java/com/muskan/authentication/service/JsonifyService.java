9c48351781ddca9c6b16d5fd240c713220e9ad72
CipherKode <kodecipher@gmail.com>
2025-01-30 23:05:20 +0530

    Updated JsonifyService

--- src/main/java/com/muskan/authentication/service/JsonifyService.java
+++ src/main/java/com/muskan/authentication/service/JsonifyService.java
@@ -1,12 +1,13 @@
 package com.muskan.authentication.service;
 
 import com.muskan.authentication.dto.request.JsonifyRequest;
+import com.muskan.authentication.dto.response.JsonifyResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JsonifyService {
 
-    public String convertToJson(JsonifyRequest jsonifyRequest) {
+    public JsonifyResponse convertToJson(JsonifyRequest jsonifyRequest) {
         return null;
     }
 }
